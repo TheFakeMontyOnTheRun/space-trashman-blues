c77c0237a05042b098f7b4cae83397c5e891d3d8
Daniel Monteiro <themontyhacker@gmail.com>
2020-10-11 22:36:01 +0100

    Add more tests about object manipulation and interactions

--- core/tests/TestObjectManipulation.cpp
+++ core/tests/TestObjectManipulation.cpp
@@ -30,12 +30,13 @@ TEST(TestObjectManipulation, canUseObjectsTogether) {
 
   item[0].description = "usableWith";
   item[0].useWithCallback = useWithCallback;
-
+  item[0].pickable = TRUE;
   item[1].description = "farofinha";
-
+  item[1].pickable = TRUE;
   item[2].description = "pamonha";
-
+  item[2].pickable = TRUE;
   item[3].description = "cocada";
+  item[3].pickable = TRUE;
 
   initStation();
 
@@ -71,9 +72,12 @@ TEST(TestObjectManipulation, canUseObjects) {
 
   item[0].description = "usable";
   item[0].weight = 5;
+  item[0].pickable = TRUE; 
   item[0].useCallback = usableCallback;
 
   item[1].description = "artificial";
+  item[1].pickable = TRUE;
+
   initStation();
   addObjectToRoom(2, &item[0]);  
   addObjectToRoom(1, &item[0]);
@@ -88,4 +92,69 @@ TEST(TestObjectManipulation, canUseObjects) {
   parseCommand("drop", "used");
 
   ASSERT_TRUE(hasItemInRoom("lss-daedalus", "used"));
+}
+
+extern Item item[25];
+
+TEST(TestObjectManipulation, blowtorchMakesThingsPickable) {
+
+  initStation();
+  item[6].pickable = FALSE;
+
+  ASSERT_TRUE(hasItemInRoom("lss-daedalus", "helmet"));
+  ASSERT_TRUE(isPlayerAtRoom("lss-daedalus"));
+  parseCommand("pick", "helmet");
+  ASSERT_TRUE(hasItemInRoom("lss-daedalus", "helmet"));
+
+  parseCommand("pick", "blowtorch");
+
+  char buffer[255];
+  strcpy(&buffer[0], "use-with blowtorch helmet");
+  char *operator1 = strtok( &buffer[0], "\n " );
+  char *operand1 = strtok( NULL, "\n ");
+  parseCommand(operator1, operand1);
+
+  parseCommand("pick", "helmet");
+  ASSERT_FALSE(hasItemInRoom("lss-daedalus", "helmet"));
+}
+
+TEST(TestObjectManipulation, usingBootsWithPlasticPipesWillProduceTheRootKeycard) {
+
+  initStation();
+  addObjectToRoom(1, &item[17]);
+
+  ASSERT_FALSE(hasItemInRoom("lss-daedalus", "root-keycard"));
+
+  parseCommand("pick", "magnetic-boots");
+
+  char buffer[255];
+  strcpy(&buffer[0], "use-with magnetic-boots plastic-pipe");
+  char *operator1 = strtok( &buffer[0], "\n " );
+  char *operand1 = strtok( NULL, "\n ");
+  parseCommand(operator1, operand1);
+
+  ASSERT_TRUE(hasItemInRoom("restroom", "root-keycard"));
+}
+
+TEST(TestObjectManipulation, cantPickUnpickableObjects) {
+
+  struct Item item;
+  memset(&item, 0, sizeof(struct Item));
+
+  item.description = "unpickable";
+  item.weight = 5;
+  item.pickable = FALSE; 
+  item.useCallback = usableCallback;
+
+  initStation();
+  addObjectToRoom(1, &item);
+
+
+  ASSERT_TRUE(hasItemInRoom("lss-daedalus", "unpickable"));
+  ASSERT_TRUE(isPlayerAtRoom("lss-daedalus"));
+  parseCommand("pick", "unpickable");
+  ASSERT_TRUE(hasItemInRoom("lss-daedalus", "unpickable"));
+  item.pickable = TRUE; 
+  parseCommand("pick", "unpickable");
+  ASSERT_FALSE(hasItemInRoom("lss-daedalus", "unpickable"));
 }
