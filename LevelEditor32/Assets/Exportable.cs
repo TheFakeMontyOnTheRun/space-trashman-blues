bfa95beb7e9ab7081f58a3122174fe99441e76eb
Daniel Monteiro <danielmonteiro@id.uff.br>
2021-01-23 19:17:05 +0000

    Add prototype for level editor
    Built on top of Unity! Yikes!

---
+++ LevelEditor32/Assets/Exportable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class Exportable : MonoBehaviour
+{
+    public float ceilingHeight;
+    public float floorHeight;
+    public float floorRepetitions;
+    public float ceilingRepetitions;
+    public Material mainMaterial;
+    public Material ceilingMaterial;
+    public Material ceilingRepetitionsMaterial;
+    public Material floorMaterial;
+    public Material floorRepetitionsMaterial;
+
+    public bool blockVisibility;
+    public bool blockMovement;
+    public bool needsAlphaTest;
+    public string representation;
+
+
+    void Update()
+    {
+
+    }
+}
