3ab285f9d631ccd4bfe68e0c1435a45c8f088e23
Daniel Monteiro <themontyhacker@gmail.com>
2021-05-29 23:04:52 +0100

    [MZ] Tweaks and refactors

--- mz_frontend/menu/src/CrawlerScreen.c
+++ mz_frontend/menu/src/CrawlerScreen.c
@@ -55,8 +55,8 @@ const char *options[4] = {
 
 #define IN_RANGE(V0, V1, V)  ((V0) <= (V) && (V) <= (V1))
 
-int8_t stencilHigh[128];
-uint8_t stencilLow[128];
+int8_t stencilHigh[XRES];
+uint8_t stencilLow[XRES];
 
 int8_t cameraX = 33;
 int8_t cameraZ = 22;
@@ -87,46 +87,46 @@ struct Pattern {
 
 const struct Projection projections[40] =
         {
-                {	0	,	63	,	-128	},	//	1
-                {	0	,	63	,	-64	},	//	2
-                {	20	,	63	,	-42	},	//	3
-                {	31	,	63	,	-32	},	//	4
-                {	37	,	63	,	-25	},	//	5
-                {	41	,	63	,	-21	},	//	6
-                {	44	,	63	,	-18	},	//	7
-                {	47	,	63	,	-16	},	//	8
-                {	48	,	63	,	-14	},	//	9
-                {	50	,	63	,	-12	},	//	10
-                {	51	,	63	,	-11	},	//	11
-                {	52	,	63	,	-10	},	//	12
-                {	53	,	63	,	-9	},	//	13
-                {	53	,	63	,	-9	},	//	14
-                {	54	,	63	,	-8	},	//	15
-                {	55	,	63	,	-8	},	//	16
-                {	55	,	63	,	-7	},	//	17
-                {	55	,	63	,	-7	},	//	18
-                {	56	,	63	,	-6	},	//	19
-                {	56	,	63	,	-6	},	//	20
-                {	56	,	63	,	-6	},	//	21
-                {	57	,	63	,	-5	},	//	22
-                {	57	,	63	,	-5	},	//	23
-                {	57	,	63	,	-5	},	//	24
-                {	57	,	63	,	-5	},	//	25
-                {	58	,	63	,	-4	},	//	26
-                {	58	,	63	,	-4	},	//	27
-                {	58	,	63	,	-4	},	//	28
-                {	58	,	63	,	-4	},	//	29
-                {	58	,	63	,	-4	},	//	30
-                {	58	,	63	,	-4	},	//	31
-                {	59	,	63	,	-4	},	//	32
-                {	59	,	63	,	-3	},	//	33
-                {	59	,	63	,	-3	},	//	34
-                {	59	,	63	,	-3	},	//	35
-                {	59	,	63	,	-3	},	//	36
-                {	59	,	63	,	-3	},	//	37
-                {	59	,	63	,	-3	},	//	38
-                {	59	,	63	,	-3	},	//	39
-                {	59	,	63	,	-3	},	//	40
+                {	0	,	128	,	-128	},	//	1
+                {	0	,	127	,	-64	},	//	2
+                {	20	,	105	,	-42	},	//	3
+                {	31	,	95	,	-32	},	//	4
+                {	37	,	88	,	-25	},	//	5
+                {	41	,	84	,	-21	},	//	6
+                {	44	,	81	,	-18	},	//	7
+                {	47	,	79	,	-16	},	//	8
+                {	48	,	77	,	-14	},	//	9
+                {	50	,	75	,	-12	},	//	10
+                {	51	,	74	,	-11	},	//	11
+                {	52	,	73	,	-10	},	//	12
+                {	53	,	72	,	-9	},	//	13
+                {	53	,	72	,	-9	},	//	14
+                {	54	,	71	,	-8	},	//	15
+                {	55	,	71	,	-8	},	//	16
+                {	55	,	70	,	-7	},	//	17
+                {	55	,	70	,	-7	},	//	18
+                {	56	,	69	,	-6	},	//	19
+                {	56	,	69	,	-6	},	//	20
+                {	56	,	69	,	-6	},	//	21
+                {	57	,	68	,	-5	},	//	22
+                {	57	,	68	,	-5	},	//	23
+                {	57	,	68	,	-5	},	//	24
+                {	57	,	68	,	-5	},	//	25
+                {	58	,	67	,	-4	},	//	26
+                {	58	,	67	,	-4	},	//	27
+                {	58	,	67	,	-4	},	//	28
+                {	58	,	67	,	-4	},	//	29
+                {	58	,	67	,	-4	},	//	30
+                {	58	,	67	,	-4	},	//	31
+                {	59	,	67	,	-4	},	//	32
+                {	59	,	66	,	-3	},	//	33
+                {	59	,	66	,	-3	},	//	34
+                {	59	,	66	,	-3	},	//	35
+                {	59	,	66	,	-3	},	//	36
+                {	59	,	66	,	-3	},	//	37
+                {	59	,	66	,	-3	},	//	38
+                {	59	,	66	,	-3	},	//	39
+                {	59	,	66	,	-3	},	//	40
         };
 
 const struct Pattern patterns[16] = {
@@ -355,7 +355,7 @@ void drawWedge(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_t dZ,
         py1z1 = z1py + ((y0 + dY) * z1dx);
     }
 
-    if (px1z1 < 0 || px0z0 > 127) {
+    if (px1z1 < 0 || px0z0 > XRESMINUSONE) {
         return;
     }
 
@@ -374,12 +374,12 @@ void drawWedge(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_t dZ,
 #ifndef FILLED_POLYS
         if (drawContour) {
 
-            if (IN_RANGE(0, 127, px0z0) && stencilHigh[px0z0] < py0z0) {
+            if (IN_RANGE(0, XRESMINUSONE, px0z0) && stencilHigh[px0z0] < py0z0) {
                 graphicsVerticalLine(px0z0, py0z0, max(py1z0, stencilHigh[px0z0]), 0);
             }
 
 
-            if (IN_RANGE(0, 127, px1z1) && py0z1 > stencilHigh[px1z1]) {
+            if (IN_RANGE(0, XRESMINUSONE, px1z1) && py0z1 > stencilHigh[px1z1]) {
                 graphicsVerticalLine(px1z1, py0z1, max(py1z1, stencilHigh[px1z1]), 0);
             }
         }
@@ -409,7 +409,7 @@ void drawWedge(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_t dZ,
 
             while ((x0 != x1 && (upperY0 != upperY1 || lowerY0 != lowerY1))) {
 
-                if (IN_RANGE(0, 127, x0)) {
+                if (IN_RANGE(0, XRESMINUSONE, x0)) {
 #ifndef FILLED_POLYS
                     if (stencilHigh[x0] <= upperY0) {
                         if (drawContour) {
@@ -440,7 +440,7 @@ void drawWedge(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_t dZ,
                     x0 += lowerSx;
                 }
 
-                if (x0 >= 128) {
+                if (x0 >= XRES) {
                     return;
                 }
 
@@ -513,7 +513,7 @@ void drawHighCubeAt(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_
     py1z0 = py0z0 + (dY * z0dx);
     py0z1 = z1py + ((y0) * z1dx);
 
-    if (px1z0 < 0 || px0z0 > 127) {
+    if (px1z0 < 0 || px0z0 > XRESMINUSONE) {
         return;
     }
 
@@ -536,18 +536,18 @@ void drawHighCubeAt(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_
 #ifndef FILLED_POLYS
         if (drawContour) {
 
-            if (IN_RANGE(0, 127, px0z0) && stencilHigh[px0z0] < py0z0) {
+            if (IN_RANGE(0, XRESMINUSONE, px0z0) && stencilHigh[px0z0] < py0z0) {
                 graphicsVerticalLine(px0z0, py0z0, stencilHigh[px0z0], 0);
             }
 
-            if (IN_RANGE(0, 127, px1z0) && stencilHigh[px1z0] < py0z0) {
+            if (IN_RANGE(0, XRESMINUSONE, px1z0) && stencilHigh[px1z0] < py0z0) {
                 graphicsVerticalLine(px1z0, py0z0, stencilHigh[px1z0], 0);
             }
-            if (IN_RANGE(0, 127, px0z1) && px0z1 < px0z0 && py0z1 > stencilHigh[px0z1]) {
+            if (IN_RANGE(0, XRESMINUSONE, px0z1) && px0z1 < px0z0 && py0z1 > stencilHigh[px0z1]) {
                 graphicsVerticalLine(px0z1, py0z1, stencilHigh[px0z1], 0);
             }
 
-            if (IN_RANGE(0, 127, px1z1) && px1z1 > px1z0 && py0z1 > stencilHigh[px1z1]) {
+            if (IN_RANGE(0, XRESMINUSONE, px1z1) && px1z1 > px1z0 && py0z1 > stencilHigh[px1z1]) {
                 graphicsVerticalLine(px1z1, py0z1, stencilHigh[px1z1], 0);
             }
 
@@ -558,7 +558,7 @@ void drawHighCubeAt(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_
         if (py0z0 > py0z1) {
             /* Ceiling is lower than camera */
             for (x = px0z0; x <= px1z0; ++x) {
-                if (IN_RANGE(0, 127, x) && stencilHigh[x] < py0z0) {
+                if (IN_RANGE(0, XRESMINUSONE, x) && stencilHigh[x] < py0z0) {
                     if (drawContour) {
 #ifdef FILLED_POLYS
                         graphicsVerticalLine(x, py0z0, stencilHigh[x], 9);
@@ -574,7 +574,7 @@ void drawHighCubeAt(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_
             /* Ceiling is higher than the camera*/
             /* Let's just draw the nearer segment */
             for (x = px0z0; x <= px1z0; ++x) {
-                if (IN_RANGE(0, 127, x) && stencilHigh[x] < py0z0) {
+                if (IN_RANGE(0, XRESMINUSONE, x) && stencilHigh[x] < py0z0) {
 #ifdef FILLED_POLYS
                     graphicsVerticalLine(x, py0z0, stencilHigh[x], 9);
 #else
@@ -602,7 +602,7 @@ void drawHighCubeAt(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_
 
             while ((x0 != x1 || y0 != y1)) {
 
-                if (IN_RANGE(0, 127, x0)) {
+                if (IN_RANGE(0, XRESMINUSONE, x0)) {
                     if (stencilHigh[x0] < y0) {
                         if (drawContour) {
 #ifdef FILLED_POLYS
@@ -624,7 +624,7 @@ void drawHighCubeAt(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_
                     x0 += sx;
                 }
 
-                if (x0 >= 128) {
+                if (x0 >= XRES) {
                     goto right_stroke;
                 }
 
@@ -674,7 +674,7 @@ void drawHighCubeAt(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_
                     x0 += sx;
                 }
 
-                if (x0 >= 128) {
+                if (x0 >= XRES) {
                     goto final_stroke;
                 }
 
@@ -863,7 +863,7 @@ void drawLowCubeAt(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_t
                     x0 += sx;
                 }
 
-                if (x0 >= 128) {
+                if (x0 >= XRES) {
                     goto right_stroke;
                 }
 
@@ -913,7 +913,7 @@ void drawLowCubeAt(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_t
                     x0 += sx;
                 }
 
-                if (x0 >= 128) {
+                if (x0 >= XRES) {
                     goto final_stroke;
                 }
 
@@ -1109,10 +1109,10 @@ void renderScene() {
         }
             break;
     }
-/*
-    for (uint8_t x = 0; x < 128; x++) {
-        graphicsVerticalLine(x, stencilHigh[x] + 1, 128, 6);
-    }*/
+
+    for (int x = 0; x < XRES; x++) {
+        graphicsVerticalLine(x, stencilHigh[x] + 1, YRESMINUSONE, 6);
+    }
 }
 
 
@@ -1130,8 +1130,8 @@ int32_t Interrogation_initStateCallback(int32_t tag, void *data) {
     cameraRotation = 0;
     viewMenu = -1;
 
-    memset(&stencilHigh[0], 0, 128);
-    memset(&stencilLow[0], 127, 128);
+    memset(&stencilHigh[0], 0, XRES);
+    memset(&stencilLow[0], 127, XRES);
 
     return 0;
 }
@@ -1168,8 +1168,8 @@ void Interrogation_initialPaintCallback() {
 
 void Interrogation_repaintCallback() {
 
-    memset(&stencilHigh[0], 0, 128);
-    memset(&stencilLow[0], 127, 128);
+    memset(&stencilHigh[0], 0, XRES);
+    memset(&stencilLow[0], 127, XRES);
 
     fill(255, 8, 8, 128, 0, TRUE);
 
@@ -1189,6 +1189,7 @@ void Interrogation_repaintCallback() {
 #ifndef AGA5BPP
     startup();
 #endif
+    
     renderScene();
 
 #ifndef AGA5BPP
@@ -1198,7 +1199,7 @@ void Interrogation_repaintCallback() {
     int farthest = 0;
 
 
-    for (int x = 0; x < 128; x += 1) {
+    for (int x = 0; x < XRES; x += 1) {
         if (farthest < stencilHigh[x]) {
             farthest = stencilHigh[x];
         }
@@ -1286,20 +1287,19 @@ int32_t Interrogation_tickCallback(int32_t tag, void *data) {
                     return kMainMenu;
 
                 case kCommandStrafeLeft:
-                    cameraX--;
+                    cameraX-=2;
                     break;
                 case kCommandStrafeRight:
-                    cameraX++;
+                    cameraX+=2;
                     break;
                 case kCommandDown:
-                    cameraZ++;
+                    cameraZ+=2;
+                    break;
+                case kCommandUp:
+                    cameraZ-=2;
                     break;
                 case kCommandFire1:
                     ++viewMenu;
-                    break;
-
-                case kCommandUp:
-                    cameraZ--;
                     break;
             }
         } else {
