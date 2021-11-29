6571d003d1ad0212659e3f6dbd00747f45d29f6e
Daniel Monteiro <monteiroquiet@gmail.com>
2021-11-29 21:32:39 +0000

    Remove all drawing, so we can better see the graphics

--- my_frontend/src/msdos.c
+++ my_frontend/src/msdos.c
@@ -109,19 +109,19 @@ void graphicsPut( uint8_t x, uint8_t y) {
 }
 
 void realPut( int x, int y, int value ) {
-    int pixel = value;
-    int px = x;
-    int py = y;
-
-    asm volatile ("movb $0x0C, %%ah\n\t"
-                  "movb %0,    %%al\n\t"
-                  "movb $0x0,  %%bh\n\t"
-                  "movw %1,    %%cx\n\t"
-                  "movw %2,    %%dx\n\t"
-                  "int $0x10"
-    :
-    :"rm" (pixel), "rm" (px), "rm" (py)
-    );
+//    int pixel = value;
+//    int px = x;
+//    int py = y;
+//
+//    asm volatile ("movb $0x0C, %%ah\n\t"
+//                  "movb %0,    %%al\n\t"
+//                  "movb $0x0,  %%bh\n\t"
+//                  "movw %1,    %%cx\n\t"
+//                  "movw %2,    %%dx\n\t"
+//                  "int $0x10"
+//    :
+//    :"rm" (pixel), "rm" (px), "rm" (py)
+//    );
 }
 
 void clearGraphics() {
@@ -154,54 +154,54 @@ uint8_t getKey() {
 }
 
 void writeStrWithLimit(int _x, int y, char *text, int limitX) {
-
-    uint8_t len = strlen(text);
-    char *ptr = text;
-    uint8_t c = 0;
-    uint8_t chary = 0;
-    uint8_t x = _x;
-
-    for (; c < len && y < 25; ++c) {
-
-        char cha = *ptr;
-
-        if (x == limitX) {
-            ++y;
-            x = _x;
-        } else if (cha == '\n') {
-            ++y;
-            x = _x;
-            ++ptr;
-            continue;
-        } else {
-            ++x;
-        }
-
-        asm volatile (
-        "movb $0x02, %%ah\n"
-        "movb    %0, %%dl\n"
-        "movb    %1, %%dh\n"
-        "movb  $0x0, %%bh\n"
-        "int  $0x10\n"
-        :
-        : "rm" (x), "rm" (y)
-        :
-        );
-
-        asm volatile (
-        "movb $0x09, %%ah\n"
-        "movb  %[c], %%al\n"
-        "movw $0x01, %%cx\n"
-        "movb  $0x0, %%bh\n"
-        "movb $0x03, %%bl\n"
-        "int $0x10\n"
-        :
-        :[c] "r"(cha)
-        :
-        );
-
-        ++ptr;
-    }
+//
+//    uint8_t len = strlen(text);
+//    char *ptr = text;
+//    uint8_t c = 0;
+//    uint8_t chary = 0;
+//    uint8_t x = _x;
+//
+//    for (; c < len && y < 25; ++c) {
+//
+//        char cha = *ptr;
+//
+//        if (x == limitX) {
+//            ++y;
+//            x = _x;
+//        } else if (cha == '\n') {
+//            ++y;
+//            x = _x;
+//            ++ptr;
+//            continue;
+//        } else {
+//            ++x;
+//        }
+//
+//        asm volatile (
+//        "movb $0x02, %%ah\n"
+//        "movb    %0, %%dl\n"
+//        "movb    %1, %%dh\n"
+//        "movb  $0x0, %%bh\n"
+//        "int  $0x10\n"
+//        :
+//        : "rm" (x), "rm" (y)
+//        :
+//        );
+//
+//        asm volatile (
+//        "movb $0x09, %%ah\n"
+//        "movb  %[c], %%al\n"
+//        "movw $0x01, %%cx\n"
+//        "movb  $0x0, %%bh\n"
+//        "movb $0x03, %%bl\n"
+//        "int $0x10\n"
+//        :
+//        :[c] "r"(cha)
+//        :
+//        );
+//
+//        ++ptr;
+//    }
 }
 
 void writeStr(uint8_t _x, uint8_t y, const char *text, uint8_t fg, uint8_t bg) {
@@ -217,29 +217,30 @@ void graphicsFlush() {
     pokeb(0xB8000, 5 * 80 + 41, 4);
     pokeb(0xB8000, 5 * 80 + 42, 5);
 
-    for ( int y = 0; y < 128; ++y ) {
-
-
-        for ( int x = 0; x < 64; ++x ) {
-
-            origin = imageBuffer[ offset ];
-
-            if ( lastOrigin != origin ) {
-                value = origin;
-                lastOrigin = origin;
-            }
-
-
-            if ( buffer[ offset ] != value ) {
-                realPut( 16 + (2 * x), (y) + 36, value);
-                realPut( 16 + (2 * x) + 1, (y) + 36, value);
-            }
-
-            buffer[ offset ] = value;
-
-            ++offset;
-        }
-    }
+//
+//    for ( int y = 0; y < 128; ++y ) {
+//
+//
+//        for ( int x = 0; x < 64; ++x ) {
+//
+//            origin = imageBuffer[ offset ];
+//
+//            if ( lastOrigin != origin ) {
+//                value = origin;
+//                lastOrigin = origin;
+//            }
+//
+//
+//            if ( buffer[ offset ] != value ) {
+//                realPut( 16 + (2 * x), (y) + 36, value);
+//                realPut( 16 + (2 * x) + 1, (y) + 36, value);
+//            }
+//
+//            buffer[ offset ] = value;
+//
+//            ++offset;
+//        }
+//    }
 
     memset( imageBuffer, 0, 64 * 128);
 }
