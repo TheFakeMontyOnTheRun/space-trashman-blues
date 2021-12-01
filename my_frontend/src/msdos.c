0090175bd9fce6c18747623f0893f6d2257bd6ad
Daniel Monteiro <monteiroquiet@gmail.com>
2021-12-01 19:33:43 +0000

    Avoid clobbering AX when writing the pixel

--- my_frontend/src/msdos.c
+++ my_frontend/src/msdos.c
@@ -148,22 +148,20 @@ void realPut( int x, int y, int value ) {
 
 
     if (y & 1) {
-        asm volatile("movw $0xb800, %%ax\n\t"
-                     "movw %%ax, %%es\n\t"
+        asm volatile("movw $0xb800, %%es\n\t"
                      "movw %0, %%di  \n\t"
                      "movb %1, %%es:(%%di)\n\t"
         :
         : "r"( ((x / 4) + ((y / 2) * 80)) ), "r" (value)
-        : "ax", "es", "di"
+        : "es", "di"
         );
     } else {
-        asm volatile("movw $0xb800, %%ax\n\t"
-                     "movw %%ax, %%es\n\t"
+        asm volatile("movw $0xb800, %%es\n\t"
                      "movw %0, %%di  \n\t"
                      "movb %1, %%es:(%%di)\n\t"
         :
         : "r"( 0x2000 + ((x / 4) + ((y / 2) * 80)) ), "r" (value)
-        : "ax", "es", "di"
+        : "es", "di"
         );
     }
 }
