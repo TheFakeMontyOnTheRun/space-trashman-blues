cae32a73d695aa8a74f1d078ab3eb86838d791c0
Daniel Monteiro <monteiroquiet@gmail.com>
2021-12-11 10:04:40 +0000

    Just one line

--- my_frontend/src/msdos.c
+++ my_frontend/src/msdos.c
@@ -344,7 +344,7 @@ void graphicsFlush() {
     int diOffset;
     uint8_t *bufferPtr = &imageBuffer[0];
 
-    for (int y = 0; y < 128; ++y) {
+    for (int y = 0; y < 1; ++y) {
 
         diOffset = ((y & 1) ? 0x2000 : 0x0) + (((y + 36) / 2) * 80) + 4;
 
