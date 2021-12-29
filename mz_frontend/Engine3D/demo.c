31d6fc442370c9132fadf633c58dedc70bebb877
Daniel Monteiro <monteiroquiet@gmail.com>
2021-12-29 10:08:31 +0000

    [MZ] Copy the colours the other way around with wedge and square

--- mz_frontend/Engine3D/demo.c
+++ mz_frontend/Engine3D/demo.c
@@ -121,9 +121,9 @@ uint8_t drawWedge(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, int8_t 
     int16_t px1z1;
 
 #ifndef USE_FILLED_POLYS
-    uint8_t shouldStipple = (z0 >= STIPPLE_DISTANCE) ? 6 : 2;
+    uint8_t shouldStipple = (z0 >= STIPPLE_DISTANCE) ? 5 : 1;
 #else
-    uint8_t shouldStipple = (z0 >= STIPPLE_DISTANCE) ? 6 : 2;
+    uint8_t shouldStipple = (z0 >= STIPPLE_DISTANCE) ? 0 : 5;
 #endif
 
     uint8_t stipple = 1;
@@ -363,9 +363,9 @@ uint8_t drawSquare(int8_t x0, int8_t y0, int8_t z0, int8_t dX, int8_t dY, uint8_
     }
 
 #ifndef USE_FILLED_POLYS
-    uint8_t shouldStipple = (z0 >= STIPPLE_DISTANCE) ? 6 : 2;
+    uint8_t shouldStipple = (z0 >= STIPPLE_DISTANCE) ? 5 : 1;
 #else
-    uint8_t shouldStipple = (z0 >= STIPPLE_DISTANCE) ? 6 : 2;
+    uint8_t shouldStipple = (z0 >= STIPPLE_DISTANCE) ? 5 : 1;
 #endif
 
     uint8_t stipple = 1;
