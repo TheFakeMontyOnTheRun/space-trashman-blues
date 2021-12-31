a4c0b9e40cb7de5883ca24b2aaf061e99e3abb2a
Daniel Monteiro <monteiroquiet@gmail.com>
2021-12-31 15:55:32 +0000

    [MY] Fix title screen handling on Mega Drive

--- my_frontend/src/smd.c
+++ my_frontend/src/smd.c
@@ -13,9 +13,11 @@ extern struct ObjectNode *roomItem;
 
 extern int accessGrantedToSafe;
 
+#define COOLDOWN_MAX 0x2EF
+
 uint8_t buffered = '.';
 int cursorPosition = 0;
-
+uint16_t cooldown;
 
 char *menuItems[] = {
         "Use/Toggle",
@@ -25,6 +27,70 @@ char *menuItems[] = {
         "Next item",
         "Next in room",
 };
+
+void performAction() {
+    struct Room *room = getRoom(getPlayerRoom());
+
+    switch (getGameStatus()) {
+        case kBadVictory:
+            showMessage("Victory! Too bad you didn't survive\nto tell the story\n\n\n\n\n\n");
+            while (1);
+
+        case kBadGameOver:
+            showMessage("You're dead! And so are millions of\n"
+                        "other people on the path of\n"
+                        "destruction faulty reactor\n\n\n\n\n\n");
+            while (1);
+
+        case kGoodVictory:
+            showMessage("Victory! You managed to destroy the\nship and get out alive\n\n\n\n\n\n");
+            while (1);
+
+        case kGoodGameOver:
+            showMessage("You failed! While you fled the ship\n"
+                        "alive, you failed to prevent the \n"
+                        "worstscenario and now EVERYBODY is\n"
+                        "dead (and that includes you!)\n\n\n\n\n");
+            while (1);
+
+        default:
+        case kNormalGameplay:
+            break;
+    }
+
+/*
+char *menuItems[] = {
+ 0       "Use/Toggle current item",
+ 1       "Use current item with...",
+ 2       "Pick",
+ 3       "Drop",
+ 4       "Next item in inventory",
+ 5       "Next room item in focus",
+};
+*/
+
+    switch (cursorPosition) {
+        case 0:
+            useObjectNamed(getItem(focusedItem->item)->name);
+            break;
+        case 1:
+            interactWithItemInRoom();
+            break;
+        case 2:
+            pickItem();
+            break;
+        case 3:
+            dropItem();
+            break;
+        case 4:
+            nextItemInHand();
+            break;
+        case 5:
+            nextItemInRoom();
+            break;
+    }
+}
+
 
 static void handleInput()
 {
@@ -38,67 +104,50 @@ static void handleInput()
 
     if (value & BUTTON_A)
     {
-        if (value & BUTTON_LEFT)
-        {
-
-        }
-        if (value & BUTTON_RIGHT)
-        {
-
+        if (value & BUTTON_LEFT) {
+            buffered = 'a';
+        } else if (value & BUTTON_RIGHT) {
+            buffered = 'd';
         }
     } else {
-        if (value & BUTTON_UP)
-        {
+
+        if (value & BUTTON_UP) {
             buffered = 'w';
         }
-        if (value & BUTTON_DOWN)
-        {
+
+        if (value & BUTTON_DOWN) {
             buffered = 's';
         }
-        if (value & BUTTON_LEFT)
-        {
+
+        if (value & BUTTON_LEFT) {
             buffered = 'q';
         }
-        if (value & BUTTON_RIGHT)
-        {
+
+        if (value & BUTTON_RIGHT) {
             buffered = 'e';
         }
-    }
-}
 
+        if (value & BUTTON_START) {
+            buffered = 'k';
+        }
 
-static void joyEvent(u16 joy, u16 changed, u16 state)
-{
-    // START button state changed
-    if (changed & BUTTON_START)
-    {
-        // START button pressed ?
-        if (state & BUTTON_START)
-        {
+        if ((value & BUTTON_B)&& !cooldown) {
+            performAction();
+            HUD_refresh();
+            cooldown = COOLDOWN_MAX;
+        }
+
+        if ((value & BUTTON_C) && !cooldown) {
+            cursorPosition = (cursorPosition + 1);
+
+            if (cursorPosition >= 6) {
+                cursorPosition = 0;
+            }
+
+            HUD_refresh();
+            cooldown = COOLDOWN_MAX;
         }
     }
-
-    if (changed & state & BUTTON_A)
-    {
-
-    }
-    if (changed & state & BUTTON_B)
-    {
-
-    }
-
-    // C button state changed
-    if (changed & BUTTON_C)
-    {
-        // C button pressed ?
-        if (state & BUTTON_C)
-        {
-
-        }
-    }
-}
-
-void fix_line (uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1) {
 }
 
 void shutdownGraphics() {
@@ -108,7 +157,7 @@ void shutdownGraphics() {
 
 
 void writeStrWithLimit(int _x, int y, char *str, int limitX) {
-    BMP_clearText(1, _x, y);
+    BMP_clearText(_x, y, strlen(str));
     BMP_drawText(str, _x, y);
 }
 
@@ -118,13 +167,6 @@ void writeStr(uint8_t _x, uint8_t y, const char *text, uint8_t fg, uint8_t bg) {
 
 void graphicsPut( uint8_t x, uint8_t y) {
     BMP_setPixel(x, y, 0x11);
-}
-
-
-void hLine(uint8_t x0, uint8_t x1, uint8_t y) {
-    for (int x = x0; x < x1; ++x ) {
-        graphicsPut( x, y);
-    }
 }
 
 void vLine(uint8_t x0, uint8_t y0, uint8_t y1, uint8_t shouldStipple) {
@@ -147,16 +189,19 @@ void vLine(uint8_t x0, uint8_t y0, uint8_t y1, uint8_t shouldStipple) {
         shouldStipple = 1;
     }
 
-
     for (int y = y0; y < y1; ++y ) {
         if (shouldStipple) {
             stipple = !stipple;
         }
 
         if (stipple) {
-            BMP_setPixel(x0, y, 0x11 + colour);
+            BMP_setPixel(x0, y, 3 + colour);
         }
     }
+}
+
+static void joyEvent(u16 joy, u16 changed, u16 state) {
+
 }
 
 void showMessage(const char *message) {
@@ -164,13 +209,17 @@ void showMessage(const char *message) {
 }
 
 void clearScreen() {
+    BMP_waitWhileFlipRequestPending();
+    BMP_clear();
 
+    for ( int c = 0; c < 20; ++c) {
+        BMP_clearText(0, c, 32);
+    }
 }
 
 void clearGraphics() {
     BMP_waitWhileFlipRequestPending();
     BMP_clear();
-
 }
 
 void drawWindow(int tx, int ty, int tw, int th, const char* title ) {}
@@ -180,25 +229,21 @@ uint8_t getKey() {
     return buffered;
 }
 
-
-
 void titleScreen() {
-//    int keepGoing = 1;
-//    clearGraphics();
-//
-//    writeStr(1, 1, "Space Mare Imperium:", 2, 0);
-//    writeStr(1, 2, "     Derelict", 2, 0);
-//    writeStr(1, 4, "by Daniel Monteiro", 2, 0);
-//    writeStr(1, 6, "  Press B button ", 2, 0);
-//    writeStr(1, 7, "    to start", 2, 0);
-
-//    while (keepGoing) {
-//        if (getKey() != '.') {
-//            keepGoing = 0;
-//        }
-//    }
-
+    int keepGoing = 1;
     clearGraphics();
+
+    writeStr(1, 1, "Space Mare Imperium:", 2, 0);
+    writeStr(1, 2, "     Derelict", 2, 0);
+    writeStr(1, 4, "by Daniel Monteiro", 2, 0);
+    writeStr(1, 7, "  Press start button ", 2, 0);
+
+    while (keepGoing) {
+        if (getKey() == 'k') {
+            keepGoing = 0;
+        }
+    }
+    clearScreen();
 }
 
 void puts(char *unused) {
@@ -228,6 +273,7 @@ void init() {
 }
 
 void graphicsFlush() {
+    HUD_initialPaint();
     BMP_flip(1);
 }
 
