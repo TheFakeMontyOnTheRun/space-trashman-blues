791d0dee8c75dd84447b7dd972b8f6777a2c9698
Daniel Monteiro <themontyhacker@gmail.com>
2022-11-24 20:53:53 +0000

    [CORE] refactor: the core interface, reorganizing functions by their use

--- core/include/Core.h
+++ core/include/Core.h
@@ -10,13 +10,8 @@ Created by Daniel Monteiro on 2019-07-26.
 #define FALSE 0
 #endif
 
-enum EGameStates {
-  kNormalGameplay,
-  kGoodVictory,
-  kBadVictory,
-  kGoodGameOver,
-  kBadGameOver
-};
+#define TOTAL_ROOMS 24
+#define TOTAL_ITEMS 39
 
 struct Item;
 
@@ -71,89 +66,32 @@ struct Room {
   uint8_t rankRequired;
 };
 
-struct Room *addRoom(
-        const char *description,
-#ifdef INCLUDE_ROOM_DESCRIPTIONS
-        const char *info,
-#endif
-        uint8_t sizeX, uint8_t sizeY, uint8_t chanceOfRandomBattle, int8_t connections[6]);
+enum EGameStates {
+	kNormalGameplay,
+	kGoodVictory,
+	kBadVictory,
+	kGoodGameOver,
+	kBadGameOver
+};
 
-struct Item* addItem(const char *description,
-#ifdef INCLUDE_ITEM_DESCRIPTIONS
-                     const char *info,
-#endif
-#ifdef ITEMS_HAVE_WEIGHT
-		uint8_t weight,
-#endif
-					 uint8_t pickable,
-					 int8_t positionX,
-					 int8_t positionY);
 
-struct WorldPosition* getPlayerPosition();
+void initCore(void);
 
-struct Item* getItem(uint8_t index);
-
-struct ObjectNode* getPlayerItems();
-
-void setPlayerPosition(struct WorldPosition* pos);
-
-void addToRoom( const char* roomName, struct Item *itemName );
-
-uint8_t isCloseToObject( struct WorldPosition* pos, struct Item* item );
-
-void addObjectToRoom(uint8_t roomId, struct Item *itemToAdd);
-
-void dropObjectToRoom(uint8_t roomId, struct Item *itemToDrop);
-
-void pickObject(struct Item *itemToPick);
-
-void pickObjectByName(const char *objName);
-
-void dropObjectByName(const char *objName);
+void setGameStatus(enum EGameStates newStatus);
 
 enum EGameStates  getGameStatus(void);
-
-struct Item *getItemNamed(const char* name);
 
 uint8_t getPlayerDirection(void);
 
 void setPlayerDirection(uint8_t direction);
 
-#ifdef INCLUDE_ITEM_DESCRIPTIONS
-void infoAboutItemNamed(const char* itemName);
-#endif
-
 void moveBy(uint8_t direction);
 
-uint8_t getPlayerRoom(void);
+struct WorldPosition* getPlayerPosition();
 
-void setPlayerLocation(uint8_t location);
+void setPlayerPosition(struct WorldPosition* pos);
 
-uint8_t hasItemInRoom(const char *roomName, const char *itemName);
-
-uint8_t playerHasObject( const char* itemName);
-
-uint8_t isPlayerAtRoom(const char *roomName);
-
-const char *getRoomDescription(void);
-
-struct Room *getRoom(uint8_t index);
-
-void useObjectNamed(const char* operand);
-
-void removeObjectFromList(struct Item *itemToRemove, struct ObjectNode *listHead);
-
-void removeObjectFromRoom(struct Item *itemToRemove);
-
-void useObjectsTogether(const char* operands);
-
-void showInventory(void);
-
-extern LogDelegate defaultLogger;
-
-void setLoggerDelegate(LogDelegate newDelegate);
-
-void setErrorHandlerCallback(ErrorHandlerCallback callback);
+uint8_t isPositionAllowed(int8_t x, int8_t y);
 
 void walkTo(const char* operands);
 
@@ -167,21 +105,85 @@ uint8_t getPlayerRank(void);
 
 void setPlayerRank(uint8_t newRank);
 
-uint8_t isPositionAllowed(int8_t x, int8_t y);
+void showInventory(void);
 
-uint8_t listIsEmpty(struct ObjectNode *listHead);
+uint8_t playerHasObject( const char* itemName);
 
-void initCore(void);
+void setPlayerLocation(uint8_t location);
 
-void setGameStatus(enum EGameStates newStatus);
+uint8_t getPlayerRoom(void);
+
+uint8_t isPlayerAtRoom(const char *roomName);
+
+struct Item* addItem(const char *description,
+#ifdef INCLUDE_ITEM_DESCRIPTIONS
+                     const char *info,
+#endif
+#ifdef ITEMS_HAVE_WEIGHT
+		uint8_t weight,
+#endif
+					 uint8_t pickable,
+					 int8_t positionX,
+					 int8_t positionY);
+
+struct ObjectNode* getPlayerItems();
+
+struct Item* getItem(uint8_t index);
+
+uint8_t isCloseToObject( struct WorldPosition* pos, struct Item* item );
+
+void pickObject(struct Item *itemToPick);
+
+void pickObjectByName(const char *objName);
+
+void dropObjectByName(const char *objName);
+
+uint8_t hasItemInRoom(const char *roomName, const char *itemName);
+
+void addToRoom( const char* roomName, struct Item *itemName );
+
+struct Room *addRoom(
+		const char *description,
+#ifdef INCLUDE_ROOM_DESCRIPTIONS
+		const char *info,
+#endif
+		uint8_t sizeX, uint8_t sizeY, uint8_t chanceOfRandomBattle, int8_t connections[6]);
+
+void addObjectToRoom(uint8_t roomId, struct Item *itemToAdd);
+
+void dropObjectToRoom(uint8_t roomId, struct Item *itemToDrop);
+
+
+struct Item *getItemNamed(const char* name);
+
+#ifdef INCLUDE_ITEM_DESCRIPTIONS
+void infoAboutItemNamed(const char* itemName);
+#endif
+
+const char *getRoomDescription(void);
+
+struct Room *getRoom(uint8_t index);
+
+void removeObjectFromRoom(struct Item *itemToRemove);
+
+void useObjectNamed(const char* operand);
+
+void useObjectsTogether(const char* operands);
+
+extern LogDelegate defaultLogger;
+
+void setLoggerDelegate(LogDelegate newDelegate);
+
+void setErrorHandlerCallback(ErrorHandlerCallback callback);
 
 void useObjectNamed(const char *operand);
 
-#define TOTAL_ROOMS 24
-#define TOTAL_ITEMS 39
+void removeObjectFromList(struct Item *itemToRemove, struct ObjectNode *listHead);
 
+uint8_t listIsEmpty(struct ObjectNode *listHead);
 
 /* TODO: make accessory method for this */
 extern uint8_t itemsCount;
+
 extern struct ObjectNode objectNodes[TOTAL_ITEMS];
 #endif
