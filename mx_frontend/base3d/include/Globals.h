d806be901981e4ad79af932f1be477c5880f1777
Daniel Monteiro <themontyhacker@gmail.com>
2022-12-11 23:19:02 +0000

    [MX] fix: global type mismatch

--- mx_frontend/base3d/include/Globals.h
+++ mx_frontend/base3d/include/Globals.h
@@ -5,6 +5,6 @@ Created by Daniel Monteiro on 03/10/2019.
 #ifndef THE_MISTRAL_REPORT_GLOBALS_H
 #define THE_MISTRAL_REPORT_GLOBALS_H
 extern const char *mainText;
-extern int32_t currentGameMenuState;
+extern enum EGameMenuState currentGameMenuState;
 extern int currentSelectedItem;
 #endif
