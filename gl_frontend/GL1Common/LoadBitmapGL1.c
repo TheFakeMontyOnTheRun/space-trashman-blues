fbb27a0184954e908e94c36790cb74bffef1356f
Daniel Monteiro <themontyhacker@gmail.com>
2022-12-11 11:01:09 +0000

    [GL] fix memory leak with the expanded texture when submitting to the GPU

--- gl_frontend/GL1Common/LoadBitmapGL1.c
+++ gl_frontend/GL1Common/LoadBitmapGL1.c
@@ -210,7 +210,11 @@ int submitBitmapToGPU(struct Bitmap* bitmap) {
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    //free( expanded );
+#ifndef N64
+    free( expanded );
+#else
+	free_uncached( expanded );
+#endif
 	glBindTexture(GL_TEXTURE_2D, 0);
 
     return newId;
