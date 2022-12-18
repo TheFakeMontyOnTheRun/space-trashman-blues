13319aae3e74149aa19b52e967589b08988485eb
Daniel Monteiro <themontyhacker@gmail.com>
2022-12-18 21:58:07 +0000

    [GL] Make the game (almost) playable on the N64 controller
    A key for switching the current item is still pending

--- gl_frontend/base3d/src/ultra/UltraGLRenderer.c
+++ gl_frontend/base3d/src/ultra/UltraGLRenderer.c
@@ -48,16 +48,16 @@ void handleSystemEvents() {
 	struct controller_data pressed = get_keys_pressed();
 	struct controller_data down = get_keys_down();
 
-	if (pressed.c[0].C_up) {
+	if (pressed.c[0].C_up || down.c[0].up ) {
 		mBufferedCommand = kCommandUp;
 	}
 
-	if (pressed.c[0].C_down) {
+	if (pressed.c[0].C_down || down.c[0].down ) {
 		mBufferedCommand = kCommandDown;
 	}
 
 	if (down.c[0].start) {
-		mBufferedCommand = kCommandFire1;
+		mBufferedCommand = kCommandBack;
 	}
 
 	if (down.c[0].A) {
@@ -65,18 +65,28 @@ void handleSystemEvents() {
 	}
 
 	if (down.c[0].B) {
-		mBufferedCommand = kCommandBack;
+		mBufferedCommand = kCommandFire2;
 	}
 
-	if (down.c[0].C_left) {
+	if (down.c[0].Z) {
+		mBufferedCommand = kCommandFire3;
+	}
+
+	if (down.c[0].L) {
+		mBufferedCommand = kCommandStrafeLeft;
+	}
+
+	if (down.c[0].R) {
+		mBufferedCommand = kCommandStrafeRight;
+	}
+
+	if (pressed.c[0].C_left|| down.c[0].left) {
 		mBufferedCommand = kCommandLeft;
 	}
 
-	if (down.c[0].C_right) {
+	if (pressed.c[0].C_right|| down.c[0].right) {
 		mBufferedCommand = kCommandRight;
 	}
-
-
 }
 
 void graphicsShutdown() {
