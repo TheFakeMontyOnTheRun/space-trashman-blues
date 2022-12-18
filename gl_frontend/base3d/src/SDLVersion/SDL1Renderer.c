85bd259596e30e388bc95a490b23550cc23bf76c
Daniel Monteiro <themontyhacker@gmail.com>
2022-12-18 19:53:23 +0000

    [GL] add SDL1 makefile, along with fixes to make it work
    Sadly, it doesn't support Emscripten (yet?).

--- gl_frontend/base3d/src/SDLVersion/SDL1Renderer.c
+++ gl_frontend/base3d/src/SDLVersion/SDL1Renderer.c
@@ -31,7 +31,8 @@
 #endif
 
 SDL_Surface *video;
-SDL_Surface *stretchedBuffer;
+
+
 //SDL_AudioDeviceID deviceId;
 //
 //char* audioFiles[] = {"./data/wav/menu_move.wav", "./data/wav/menu_select.wav", "./data/wav/gotclue.wav", "./data/wav/detected2.wav", "./data/wav/bong.wav", "./data/wav/fire.wav", "./data/wav/enemyfire.wav", "./data/wav/detected2.wav" };
@@ -71,43 +72,17 @@ void enterFullScreenMode() {
 #endif
 
 
-uint8_t getPaletteEntry(uint32_t origin) {
-  uint8_t shade;
-
-  if (!(origin & 0xFF000000)) {
-    return TRANSPARENCY_COLOR;
-  }
-
-  shade = 0;
-  shade += (((((origin & 0x0000FF)) << 2) >> 8)) << 6;
-  shade += (((((origin & 0x00FF00) >> 8) << 3) >> 8)) << 3;
-  shade += (((((origin & 0xFF0000) >> 16) << 3) >> 8)) << 0;
-
-  return shade;
-}
-
 void graphicsInit() {
-  int r, g, b, c;
+
 
   SDL_Init(SDL_INIT_EVERYTHING);
-  video = SDL_SetVideoMode(320, 240, 32, 0);
-  for (r = 0; r < 256; r += 16) {
-    for (g = 0; g < 256; g += 8) {
-      for (b = 0; b < 256; b += 8) {
-	uint32_t pixel = 0xFF000000 + (r << 16) + (g << 8) + (b);
-	uint8_t paletteEntry = getPaletteEntry(pixel);
-	palette[paletteEntry] = SDL_MapRGB(video->format, (((pixel & 0x000000FF))) - 0x38,
-                                       (((pixel & 0x0000FF00) >> 8)) - 0x18,
-                                       (((pixel & 0x00FF0000) >> 16)) - 0x10);
-      }
-    }
-  }
+  video = SDL_SetVideoMode(640, 480, 32, SDL_OPENGL);
 
 #ifdef __EMSCRIPTEN__
   enterFullScreenMode ();
 #endif
   defaultFont = loadBitmap("font.img");
-	enableSmoothMovement = TRUE;
+  enableSmoothMovement = TRUE;
 
 //    if ( Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 ) == -1 ) {
 //        audioEnabled = 0;
@@ -119,8 +94,6 @@ void graphicsInit() {
 //
 //        audioEnabled = 1;
 //    }
-
-    stretchedBuffer = SDL_CreateRGBSurface(SDL_SWSURFACE, 320, 240, 32, video->format->Rmask, video->format->Gmask, video->format->Bmask, video->format->Rmask);
 }
 
 void handleSystemEvents() {
@@ -209,11 +182,9 @@ void handleSystemEvents() {
 	break;
 
       case SDLK_j:
-	useDither = FALSE;
 	visibilityCached = FALSE;
 	break;
       case SDLK_k:
-	useDither = TRUE;
 	visibilityCached = FALSE;
 	break;
 
@@ -267,60 +238,11 @@ void graphicsShutdown() {
 
 void flipRenderer() {
 
-    int x = 0, y = 0;
-    int dstY = 0;
-    int scaller = 0;
-    int even = 0;
-    int heightY = 1;
-
-    uint8_t *src;
-    uint32_t *dst;
-
-
-    SDL_LockSurface(stretchedBuffer);
-
-
-
-
-    for (y = 0; y < 200; ++y) {
-        if (scaller == 4 ) {
-            heightY = 2;
-        } else {
-            heightY = 1;
-        }
-
-
-        for (int chunky = 0; chunky < heightY; ++chunky ) {
-            dst = (uint32_t*)stretchedBuffer->pixels;
-            src = &framebuffer[(320 * y)];
-            dst += (320 * (dstY + chunky));
-
-            for (x = 0; x < 320; ++x) {
-                *dst++ = palette[*src++];
-            }
-        }
-
-        dstY++;
-        scaller++;
-
-
-        if (scaller == 5 ) {
-            scaller = 0;
-            dstY++;
-        }
-    }
-
-    SDL_UnlockSurface(stretchedBuffer);
-    SDL_BlitSurface( stretchedBuffer, NULL, video, NULL);
-
-    SDL_Flip(video);
+  SDL_GL_SwapBuffers();
+  
 #ifndef __EMSCRIPTEN__
     SDL_Delay(1000 / 60);
 #endif
 
 
-}
-
-void clearRenderer() {
-  SDL_FillRect(video, NULL, 0);
 }
