20d768e481959171b1f0979d8f7a3361940ee1fb
Daniel Monteiro <themontyhacker@gmail.com>
2023-01-20 20:00:13 +0000

    [GL] Delete the GL texture as we free the memory

--- gl_frontend/GL1Common/LoadBitmapGL1.c
+++ gl_frontend/GL1Common/LoadBitmapGL1.c
@@ -346,6 +346,9 @@ struct Bitmap *loadBitmap(const char *filename) {
 
 void releaseBitmap(struct Bitmap *ptr) {
 	assert(ptr != NULL);
+
+	glDeleteTextures( 1, &ptr->uploadId);
+
 #ifndef N64
 	free(ptr->data);
 	free(ptr);
