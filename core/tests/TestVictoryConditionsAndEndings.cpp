0e264d88955db9a81fea5c285b947ed163929046
Daniel Monteiro <themontyhacker@gmail.com>
2023-03-12 12:00:09 +0000

    [CORE] Revamp victory conditions tests
    Its not passing yet, but its not crashing, at least.

--- core/tests/TestVictoryConditionsAndEndings.cpp
+++ core/tests/TestVictoryConditionsAndEndings.cpp
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <assert.h>
 
 extern "C" {
 #include "Core.h"
@@ -15,20 +16,31 @@ extern struct ObjectNode *collectedObject;
 
 using testing::Eq;
 
-extern struct Item item[25];
+extern struct Item item[TOTAL_ITEMS];
+extern struct Room rooms[TOTAL_ROOMS];
 
-/*
+int getRoomIdByName(const char* name) {
+
+  int c = 1;
+  for (; c < TOTAL_ROOMS; ++c) {
+    if (!strcmp(rooms[c].name, name )) {
+      return c;
+    }
+  }
+  assert(FALSE);
+  return 0;
+}
 
 TEST(TestVictoryCondition, activatingTheRemoteControlWithTheActiveBombWillExplodeEverything) {
 
   initStation();
   ASSERT_EQ(getPlayerRoom(), 1);
+  
+  addObjectToRoom(getRoomIdByName("reactor-core"), getItemNamed("emp-bomb"));
+  addObjectToRoom(getRoomIdByName("lss-daedalus"), getItemNamed("fuel-rods"));
 
-  addObjectToRoom(21, &item[0]);
-  addObjectToRoom(1, &item[21]);
-
-  ASSERT_TRUE(hasItemInRoom("lab-1", "time-bomb"));
-  ASSERT_FALSE(hasItemInRoom("lab-1", "metal-mending"));
+  ASSERT_TRUE(hasItemInRoom("reactor-core", "emp-bomb"));
+  ASSERT_FALSE(hasItemInRoom("reactor-core", "fuel-rods"));
 
   ASSERT_EQ(kNormalGameplay, getGameStatus());
   item[1].useCallback(NULL);
@@ -40,68 +52,68 @@ TEST(TestVictoryCondition, activatingTheRemoteControlWithTheActiveBombWillExplod
 TEST(TestVictoryCondition, placingTheBombOnLab1WithoutTheMendingWillExplodeStationWhileAtNotAtShipWillCauseYouToGoDownWithIt) {
 
   initStation();
-  parseCommand("pick", "magnetic-boots");
-  parseCommand("use", "magnetic-boots");
 
   parseCommand("move", "0");
-  ASSERT_NE(getPlayerRoom(), 1);
+  ASSERT_FALSE(isPlayerAtRoom("lss-daedalus"));
 
-  addObjectToRoom(21, &item[0]);
-  addObjectToRoom(1, &item[21]);
-
-  ASSERT_TRUE(hasItemInRoom("lab-1", "time-bomb"));
-  ASSERT_FALSE(hasItemInRoom("lab-1", "metal-mending"));
-
+  addObjectToRoom(getRoomIdByName("reactor-core"), getItemNamed("emp-bomb"));
+  addObjectToRoom(getRoomIdByName("lss-daedalus"), getItemNamed("fuel-rods"));
+  
+  ASSERT_TRUE(hasItemInRoom("reactor-core", "emp-bomb"));
+  ASSERT_FALSE(hasItemInRoom("reactor-core", "fuel-rods"));
+ 
   ASSERT_EQ(kNormalGameplay, getGameStatus());
-  item[0].useCallback(NULL);
+  getItemNamed("emp-controller")->useCallback(NULL);
   ASSERT_EQ(kBadVictory, getGameStatus());
 }
 
 TEST(TestVictoryCondition, placingTheBombOnLab1WithoutTheMendingWillExplodeStationWhileAtShipWillMeanTheMissionIsAccomplished) {
 
   initStation();
-  ASSERT_EQ(getPlayerRoom(), 1);
 
-  addObjectToRoom(21, &item[0]);
-  addObjectToRoom(1, &item[21]);
+  ASSERT_TRUE(isPlayerAtRoom("lss-daedalus"));
 
-  ASSERT_TRUE(hasItemInRoom("lab-1", "time-bomb"));
-  ASSERT_FALSE(hasItemInRoom("lab-1", "metal-mending"));
+  addObjectToRoom(getRoomIdByName("reactor-core"), getItemNamed("emp-bomb"));
+  addObjectToRoom(getRoomIdByName("lss-daedalus"), getItemNamed("fuel-rods"));
+  
+  ASSERT_TRUE(hasItemInRoom("reactor-core", "emp-bomb"));
+  ASSERT_FALSE(hasItemInRoom("reactor-core", "fuel-rods"));
 
   ASSERT_EQ(kNormalGameplay, getGameStatus());
-  item[0].useCallback(NULL);
+  getItemNamed("emp-controller")->useCallback(NULL);
   ASSERT_EQ(kGoodVictory, getGameStatus());
+  
 }
 
 TEST(TestVictoryCondition, placingTheBombOnLab1WithTheMendingWillFailToExplodeStationProperlyWithYouAtTheShipSurvivingAndLosingYourLicense) {
 
   initStation();
-  ASSERT_EQ(getPlayerRoom(), 1);
 
-  addObjectToRoom(21, &item[0]);
-  ASSERT_TRUE(hasItemInRoom("lab-1", "time-bomb"));
-  ASSERT_TRUE(hasItemInRoom("lab-1", "metal-mending"));
+  ASSERT_TRUE(isPlayerAtRoom("lss-daedalus"));
+
+  addObjectToRoom(getRoomIdByName("reactor-core"), getItemNamed("emp-bomb"));
+
+  ASSERT_TRUE(hasItemInRoom("reactor-core", "emp-bomb"));
+  ASSERT_TRUE(hasItemInRoom("reactor-core", "fuel-rods"));
 
   ASSERT_EQ(kNormalGameplay, getGameStatus());
-  item[0].useCallback(NULL);
+  getItemNamed("emp-controller")->useCallback(NULL);
   ASSERT_EQ(kGoodGameOver, getGameStatus());
 }
 
 TEST(TestVictoryCondition, placingTheBombOnLab1WithTheMendingWillFailToExplodeStationWithYouOutsideTheShipWillCauseYouToGoDownWithIt) {
 
   initStation();
-  parseCommand("pick", "magnetic-boots");
-  parseCommand("use", "magnetic-boots");
   parseCommand("move", "0");
-  ASSERT_NE(getPlayerRoom(), 1);
 
-  addObjectToRoom(21, &item[0]);
-  ASSERT_TRUE(hasItemInRoom("lab-1", "time-bomb"));
-  ASSERT_TRUE(hasItemInRoom("lab-1", "metal-mending"));
+  ASSERT_FALSE(isPlayerAtRoom("lss-daedalus"));
+
+  addObjectToRoom(getRoomIdByName("reactor-core"), getItemNamed("emp-bomb"));
+  
+  ASSERT_TRUE(hasItemInRoom("reactor-core", "emp-bomb"));
+  ASSERT_TRUE(hasItemInRoom("reactor-core", "fuel-rods"));
 
   ASSERT_EQ(kNormalGameplay, getGameStatus());
-  item[0].useCallback(NULL);
+  getItemNamed("emp-controller")->useCallback(NULL);
   ASSERT_EQ(kBadGameOver, getGameStatus());
 }
-
-*/
