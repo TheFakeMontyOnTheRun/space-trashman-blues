c29f54bcff95e3f2f05248a1189fa9cb2872179f
Daniel Monteiro <themontyhacker@gmail.com>
2023-03-25 17:58:59 +0000

    [EE] Enable texture repeating for polygons

--- ee_frontend/base3d/src/ee/EmotionEngineRenderer.c
+++ ee_frontend/base3d/src/ee/EmotionEngineRenderer.c
@@ -21,6 +21,15 @@
 #include <kernel.h>
 #include <sifrpc.h>
 #include <loadfile.h>
+#include <dma_tags.h>
+#include <gif_tags.h>
+
+#include <gs_privileged.h>
+#include <gs_gp.h>
+#include <gs_psm.h>
+
+#include <draw.h>
+#include <draw2d.h>
 
 
 #include "libpad.h"
@@ -80,6 +89,115 @@ u32 old_pad = 0;
 u32 new_pad;
 
 
+qword_t *draw_setup_environment_with_repeat(qword_t *q, int context, framebuffer_t *frame, zbuffer_t *z)
+{
+
+	// Change this if modifying the gif packet after the giftag.
+	int qword_count = 15;
+
+	atest_t atest;
+	dtest_t  dtest;
+	ztest_t ztest;
+	blend_t blend;
+	texwrap_t wrap;
+
+	atest.enable = DRAW_ENABLE;
+	atest.method = ATEST_METHOD_NOTEQUAL;
+	atest.compval = 0x00;
+	atest.keep = ATEST_KEEP_FRAMEBUFFER;
+
+	dtest.enable = DRAW_DISABLE;
+	dtest.pass = DRAW_DISABLE;
+
+	// Enable or Disable ZBuffer
+	if (z->enable)
+	{
+		ztest.enable = DRAW_ENABLE;
+		ztest.method = z->method;
+	}
+	else
+	{
+		z->mask = 1;
+		ztest.enable = DRAW_ENABLE;
+		ztest.method = ZTEST_METHOD_ALLPASS;
+	}
+
+	// Setup alpha blending
+	blend.color1 = BLEND_COLOR_SOURCE;
+	blend.color2 = BLEND_COLOR_DEST;
+	blend.alpha  = BLEND_ALPHA_SOURCE;
+	blend.color3 = BLEND_COLOR_DEST;
+	blend.fixed_alpha = 0x80;
+
+	// Setup whole texture clamping
+	wrap.horizontal = WRAP_REPEAT;
+	wrap.vertical = WRAP_REPEAT;
+	wrap.minu = wrap.maxu = 0;
+	wrap.minv = wrap.maxv = 0;
+
+	// Begin packed gif data packet with another qword.
+	PACK_GIFTAG(q,GIF_SET_TAG(qword_count,0,0,0,GIF_FLG_PACKED,1),GIF_REG_AD);
+	q++;
+	// Framebuffer setting
+	PACK_GIFTAG(q, GS_SET_FRAME(frame->address>>11,frame->width>>6,frame->psm,frame->mask), GS_REG_FRAME + context);
+	q++;
+	// ZBuffer setting
+	PACK_GIFTAG(q, GS_SET_ZBUF(z->address>>11,z->zsm,z->mask), GS_REG_ZBUF + context);
+	q++;
+	// Override Primitive Control
+	PACK_GIFTAG(q, GS_SET_PRMODECONT(PRIM_OVERRIDE_DISABLE),GS_REG_PRMODECONT);
+	q++;
+	// Primitive coordinate offsets
+	PACK_GIFTAG(q, GS_SET_XYOFFSET(ftoi4(2048.0f),ftoi4(2048.0f)), GS_REG_XYOFFSET + context);
+	q++;
+	// Scissoring area
+	PACK_GIFTAG(q, GS_SET_SCISSOR(0,frame->width-1,0,frame->height-1), GS_REG_SCISSOR + context);
+	q++;
+	// Pixel testing
+	PACK_GIFTAG(q, GS_SET_TEST(atest.enable,atest.method,atest.compval,atest.keep,
+							   dtest.enable,dtest.pass,
+							   ztest.enable,ztest.method), GS_REG_TEST + context);
+	q++;
+	// Fog Color
+	PACK_GIFTAG(q, GS_SET_FOGCOL(0,0,0), GS_REG_FOGCOL);
+	q++;
+	// Per-pixel Alpha Blending (Blends if MSB of ALPHA is true)
+	PACK_GIFTAG(q, GS_SET_PABE(DRAW_DISABLE), GS_REG_PABE);
+	q++;
+	// Alpha Blending
+	PACK_GIFTAG(q, GS_SET_ALPHA(blend.color1,blend.color2,blend.alpha,
+								blend.color3,blend.fixed_alpha), GS_REG_ALPHA + context);
+	q++;
+	// Dithering
+	PACK_GIFTAG(q, GS_SET_DTHE(GS_DISABLE), GS_REG_DTHE);
+	q++;
+	PACK_GIFTAG(q, GS_SET_DIMX(4,2,5,3,0,6,1,7,5,3,4,2,1,7,0,6), GS_REG_DIMX);
+	q++;
+	// Color Clamp
+	PACK_GIFTAG(q,GS_SET_COLCLAMP(GS_ENABLE),GS_REG_COLCLAMP);
+	q++;
+	// Alpha Correction
+	if ((frame->psm == GS_PSM_16) || (frame->psm == GS_PSM_16S))
+	{
+		PACK_GIFTAG(q,GS_SET_FBA(ALPHA_CORRECT_RGBA16),GS_REG_FBA + context);
+		q++;
+	}
+	else
+	{
+		PACK_GIFTAG(q,GS_SET_FBA(ALPHA_CORRECT_RGBA32),GS_REG_FBA + context);
+		q++;
+	}
+	// Texture wrapping/clamping
+	PACK_GIFTAG(q, GS_SET_CLAMP(wrap.horizontal,wrap.vertical,wrap.minu,
+								wrap.maxu,wrap.minv,wrap.maxv), GS_REG_CLAMP + context);
+	q++;
+	PACK_GIFTAG(q, GS_SET_TEXA(0x80,ALPHA_EXPAND_NORMAL,0x80),GS_REG_TEXA);
+	q++;
+
+	return q;
+
+}
+
 void init_gs() {
 
 	// Define a 32-bit 640x512 framebuffer.
@@ -113,7 +231,7 @@ void init_drawing_environment() {
 	qword_t *q = packet->data;
 
 	// This will setup a default drawing environment.
-	q = draw_setup_environment(q, 0, &frame, &zBuffer);
+	q = draw_setup_environment_with_repeat(q, 0, &frame, &zBuffer);
 
 	// Now reset the primitive origin to 2048-width/2,2048-height/2.
 	q = draw_primitive_xyoffset(q, 0, (2048 - 320), (2048 - 256));
