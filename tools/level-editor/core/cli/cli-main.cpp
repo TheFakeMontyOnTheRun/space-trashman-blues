c50c3bf7be244f32a0a5ab2097b7e1e756019417
Daniel Monteiro <themontyhacker@gmail.com>
2023-12-05 08:26:09 +0000

    feat: basic editor features

--- tools/level-editor/core/cli/cli-main.cpp
+++ tools/level-editor/core/cli/cli-main.cpp
@@ -2,25 +2,26 @@
 #include <cstdint>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 #include "editor-core.h"
 
 #include <iostream>
 
 int main(int argc, char** argv) {
   bool quit = false;
-  Map map;
-  std::cout << "The most amazing level editor ever!" << std::endl;
+  editor::Map map;
 
   while (!quit) {
     std::string input;
+
+    std::cout << to_string(map) << std::endl;
+    
     std::cout << ">";
     std::cin >> input;
 
     if ("q" == input || "quit" == input) {
       quit = true;
-    }
-    
-    if ("set-connection" == input) {
+    } else if ("set-connection" == input) {
       int slot;
       uint8_t connection;
       std::cin >> slot;
@@ -28,6 +29,17 @@ int main(int argc, char** argv) {
       
       map.setConnetion(slot, connection);
       std::cout << "connecting to " << connection << " over " << slot << std::endl;
+    } else if ("create-area" == input) {
+      int x0, dx, y0, dy, z0, dz;
+
+      std::cin >> x0;
+      std::cin >> y0;
+      std::cin >> z0;
+      std::cin >> dx;
+      std::cin >> dy;
+      std::cin >> dz;
+
+      map.createArea(x0, y0, z0, dx, dy, dz);
     }
     
   }
