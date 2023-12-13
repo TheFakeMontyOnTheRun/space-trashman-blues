ea135b6c936b066fdb581744958040f383d3d345
Daniel Monteiro <themontyhacker@gmail.com>
2023-12-13 22:37:02 +0000

    [EE] Add basic skeleton for GL3 version

---
+++ ee_frontend/base3d/src/es/ESRenderer.c
@@ -0,0 +1,220 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "Common.h"
+#include "Enums.h"
+#include "FixP.h"
+#include "Vec.h"
+#include "Globals.h"
+#include "Vec.h"
+#include "LoadBitmap.h"
+#include "CActor.h"
+#include "Core.h"
+#include "Engine.h"
+#include "Dungeon.h"
+#include "MapWithCharKey.h"
+#include "CTile3DProperties.h"
+#include "CRenderer.h"
+
+#define GL_GLEXT_PROTOTYPES
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+#include <stdio.h>
+
+typedef float t_mat4x4[16];
+
+static inline void mat4x4_ortho( t_mat4x4 out, float left, float right, float bottom, float top, float znear, float zfar )
+{
+    #define T(a, b) (a * 4 + b)
+
+    out[T(0,0)] = 2.0f / (right - left);
+    out[T(0,1)] = 0.0f;
+    out[T(0,2)] = 0.0f;
+    out[T(0,3)] = 0.0f;
+
+    out[T(1,1)] = 2.0f / (top - bottom);
+    out[T(1,0)] = 0.0f;
+    out[T(1,2)] = 0.0f;
+    out[T(1,3)] = 0.0f;
+
+    out[T(2,2)] = -2.0f / (zfar - znear);
+    out[T(2,0)] = 0.0f;
+    out[T(2,1)] = 0.0f;
+    out[T(2,3)] = 0.0f;
+
+    out[T(3,0)] = -(right + left) / (right - left);
+    out[T(3,1)] = -(top + bottom) / (top - bottom);
+    out[T(3,2)] = -(zfar + znear) / (zfar - znear);
+    out[T(3,3)] = 1.0f;
+
+    #undef T
+}
+
+
+
+static const char * vertex_shader =
+    "#version 330\n"
+    "in vec2 i_position;\n"
+    "in vec4 i_color;\n"
+    "out vec4 v_color;\n"
+    "uniform mat4 u_projection_matrix;\n"
+    "void main() {\n"
+    "    v_color = i_color;\n"
+    "    gl_Position = u_projection_matrix * vec4( i_position, 0.0, 1.0 );\n"
+    "}\n";
+
+static const char * fragment_shader =
+    "#version 330\n"
+    "in vec4 v_color;\n"
+    "out vec4 o_color;\n"
+    "void main() {\n"
+    "    o_color = v_color;\n"
+    "}\n";
+
+typedef enum t_attrib_id
+{
+    attrib_position,
+    attrib_color
+} t_attrib_id;
+
+int snapshotSignal = '.';
+int needsToRedrawHUD = TRUE;
+int enable3DRendering = TRUE;
+
+static const int width = 320;
+static const int height = 240;
+SDL_Window * window;
+SDL_GLContext context;
+GLuint vs, fs, program;
+GLuint vao, vbo;
+t_mat4x4 projection_matrix;
+
+const GLfloat g_vertex_buffer_data[] = {
+/*  R, G, B, A, X, Y  */
+    1, 0, 0, 1, 0, 0,
+    0, 1, 0, 1, width, 0,
+    0, 0, 1, 1, width, height,
+
+    1, 0, 0, 1, 0, 0,
+    0, 0, 1, 1, width, height,
+    1, 1, 1, 1, 0, height
+};
+
+void graphicsInit() {
+
+    enableSmoothMovement = TRUE;
+    defaultFont = NULL;
+    
+    
+    SDL_Init( SDL_INIT_VIDEO );
+    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+    SDL_GL_SetAttribute( SDL_GL_ACCELERATED_VISUAL, 1 );
+    SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 8 );
+
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 0 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+
+
+    window = SDL_CreateWindow( "", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
+    context = SDL_GL_CreateContext( window );
+
+    vs = glCreateShader( GL_VERTEX_SHADER );
+    fs = glCreateShader( GL_FRAGMENT_SHADER );
+
+    int length = strlen( vertex_shader );
+    glShaderSource( vs, 1, ( const GLchar ** )&vertex_shader, &length );
+    glCompileShader( vs );
+
+    GLint status;
+    glGetShaderiv( vs, GL_COMPILE_STATUS, &status );
+    if( status == GL_FALSE )
+    {
+        fprintf( stderr, "vertex shader compilation failed\n" );
+        exit(0);
+    }
+
+    length = strlen( fragment_shader );
+    glShaderSource( fs, 1, ( const GLchar ** )&fragment_shader, &length );
+    glCompileShader( fs );
+
+    glGetShaderiv( fs, GL_COMPILE_STATUS, &status );
+    if( status == GL_FALSE )
+    {
+        fprintf( stderr, "fragment shader compilation failed\n" );
+        exit(0);
+    }
+
+    program = glCreateProgram();
+    glAttachShader( program, vs );
+    glAttachShader( program, fs );
+
+    glBindAttribLocation( program, attrib_position, "i_position" );
+    glBindAttribLocation( program, attrib_color, "i_color" );
+    glLinkProgram( program );
+
+    glUseProgram( program );
+
+    glDisable( GL_DEPTH_TEST );
+    glClearColor( 0.5, 0.0, 0.0, 0.0 );
+    glViewport( 0, 0, width, height );
+
+    glGenVertexArrays( 1, &vao );
+    glGenBuffers( 1, &vbo );
+    glBindVertexArray( vao );
+    glBindBuffer( GL_ARRAY_BUFFER, vbo );
+
+    glEnableVertexAttribArray( attrib_position );
+    glEnableVertexAttribArray( attrib_color );
+
+    glVertexAttribPointer( attrib_color, 4, GL_FLOAT, GL_FALSE, sizeof( float ) * 6, 0 );
+    glVertexAttribPointer( attrib_position, 2, GL_FLOAT, GL_FALSE, sizeof( float ) * 6, ( void * )(4 * sizeof(float)) );
+
+
+    glBufferData( GL_ARRAY_BUFFER, sizeof( g_vertex_buffer_data ), g_vertex_buffer_data, GL_STATIC_DRAW );
+
+    mat4x4_ortho( projection_matrix, 0.0f, (float)width, (float)height, 0.0f, 0.0f, 100.0f );
+    glUniformMatrix4fv( glGetUniformLocation( program, "u_projection_matrix" ), 1, GL_FALSE, projection_matrix );
+}
+
+void handleSystemEvents() {
+    SDL_Event event;
+    while( SDL_PollEvent( &event ) )
+    {
+        switch( event.type )
+        {
+            case SDL_KEYUP:
+                if( event.key.keysym.sym == SDLK_ESCAPE )
+                    return;
+                break;
+        }
+    }
+
+}
+
+void graphicsShutdown() {
+    texturesUsed = 0;
+    
+    SDL_GL_DeleteContext( context );
+    SDL_DestroyWindow( window );
+    SDL_Quit();
+}
+
+void flipRenderer() {
+    
+    glBindVertexArray( vao );
+    glDrawArrays( GL_TRIANGLES, 0, 6 );
+
+    SDL_GL_SwapWindow( window );
+    SDL_Delay( 1 );
+}
---
+++ ee_frontend/base3d/src/es/LoadBitmap.c
@@ -0,0 +1,120 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "Core.h"
+#include "Enums.h"
+#include "FixP.h"
+#include "Common.h"
+#include "LoadBitmap.h"
+#include "Vec.h"
+#include "CActor.h"
+#include "Dungeon.h"
+#include "PackedFileReader.h"
+#include "MapWithCharKey.h"
+#include "PackedFileReader.h"
+#include "CTile3DProperties.h"
+#include "CRenderer.h"
+
+#include "Engine.h"
+#include "FixP.h"
+#include "VisibilityStrategy.h"
+
+void bindTexture(struct Bitmap *bitmap) {
+
+}
+
+
+int submitBitmapToGPU(struct Bitmap *bitmap) {
+
+
+    return 0;
+}
+
+struct Bitmap *loadBitmap(const char *filename) {
+    size_t c;
+    uint8_t d;
+    struct StaticBuffer src = loadBinaryFileFromPath(filename);
+
+    struct Bitmap *toReturn =
+            (struct Bitmap *) calloc(1, sizeof(struct Bitmap));
+
+    size_t sizeInDisk = src.size - 4; //total size minus the header
+
+    uint16_t tmp;
+    const uint8_t *ptr = src.data;
+
+    tmp = *ptr++;
+    toReturn->width = (tmp & 0xFF) << 8;
+    tmp = *ptr++;
+    toReturn->width += tmp & 0xFF;
+
+    tmp = *ptr++;
+    toReturn->height = (tmp & 0xFF) << 8;
+    tmp = *ptr++;
+    toReturn->height += tmp & 0xFF;
+
+    size_t size = toReturn->width * toReturn->height * sizeof(BitmapPixelFormat);
+
+    uint8_t *buffer = (uint8_t *) calloc(1, sizeInDisk);
+
+    memCopyToFrom(buffer, ptr, sizeInDisk);
+
+    toReturn->data = (TexturePixelFormat *) calloc(1, size);
+
+    uint8_t repetitions;
+    int pixelIndex = 0;
+    BitmapPixelFormat pixel;
+
+    for (c = 0; c < sizeInDisk; c += 5) {
+        pixel = 0;
+
+        if (buffer[c + 3] < 255) {
+            pixel = TRANSPARENCY_COLOR;
+        } else {
+            pixel += (0x80) << 24;
+            pixel += buffer[c + 2] << 16;
+            pixel += buffer[c + 1] << 8;
+            pixel += buffer[c + 0] << 0;
+        }
+        repetitions = buffer[c + 4];
+
+        for (d = 0; d < repetitions; ++d) {
+            toReturn->data[pixelIndex++] = pixel;
+        }
+    }
+
+    disposeMem(buffer);
+    disposeDiskBuffer(src);
+
+    toReturn->uploadId = -1;
+
+    return toReturn;
+}
+
+void releaseBitmap(struct Bitmap *ptr) {
+    assert(ptr != NULL);
+
+    if (ptr->nativeBuffer != NULL) {
+        disposeMem(ptr->nativeBuffer);
+    }
+
+    disposeMem(ptr->data);
+    disposeMem(ptr);
+}
+
+FixP_t lerpFix(const FixP_t v0, const FixP_t v1, const FixP_t dt, const FixP_t total) {
+    FixP_t delta = (v1 - v0);
+    FixP_t progress = Div(dt, total);
+    FixP_t reach = Mul(delta, progress);
+
+    return (v0 + reach);
+}
+
+int lerpInt(const int v0, const int v1, const long t, const long total) {
+    return fixToInt(lerpFix(intToFix(v0), intToFix(v1), intToFix(t),
+                            intToFix(total)));
+}
+
