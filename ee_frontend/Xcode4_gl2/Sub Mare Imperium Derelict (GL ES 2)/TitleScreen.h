675e445d826e198b78adf62823ef6dbd8a29222a
Daniel Monteiro <themontyhacker@gmail.com>
2024-01-04 18:49:50 +0000

    [EE] Add some basic title screen to iOS version

---
+++ ee_frontend/Xcode4_gl2/Sub Mare Imperium Derelict (GL ES 2)/TitleScreen.h
@@ -0,0 +1,17 @@
+//
+//  TitleScreen.h
+//  Derelict128
+//
+//  Created by Daniel Monteiro on 1/4/24.
+//  Copyright (c) 2024 Daniel Monteiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TitleScreen : UIViewController {
+    
+}
+
+    - (IBAction)playGame:(id)sender;
+
+@end
