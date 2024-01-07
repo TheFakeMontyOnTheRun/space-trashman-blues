6658861ca46ff6fe4eb2de3f4b20c33dec0d6c9d
Daniel Monteiro <themontyhacker@gmail.com>
2024-01-07 14:44:33 +0000

    [MY] Remove sounds from menu movement

--- my_frontend/src/UI.c
+++ my_frontend/src/UI.c
@@ -10,7 +10,6 @@
 #include "Derelict.h"
 #include "Renderer.h"
 #include "Engine.h"
-#include "SoundSystem.h"
 
 extern uint8_t playerLocation;
 
@@ -165,11 +164,9 @@ enum EGameMenuState handleCursor(const enum EGameMenuState* options, uint8_t opt
         case kCommandBack:
             return backState;
         case kCommandUp:
-            playSound(2);
             --cursorPosition;
             break;
         case kCommandDown:
-            playSound(2);
             ++cursorPosition;
             break;
         case kCommandFire1:
