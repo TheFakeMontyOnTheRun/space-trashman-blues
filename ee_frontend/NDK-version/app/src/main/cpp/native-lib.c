9b69690a5c6ba0c327b4ab503b733df7ce049c7b
Daniel Monteiro <monteiroquiet@gmail.com>
2024-02-26 21:19:55 +0000

    [EE] Fix Android builds

--- ee_frontend/NDK-version/app/src/main/cpp/native-lib.c
+++ ee_frontend/NDK-version/app/src/main/cpp/native-lib.c
@@ -181,11 +181,11 @@ Java_pt_b13h_derelictgles2_DerelictJNI_init(JNIEnv *env, jclass clazz, jint jwid
 JNIEXPORT void JNICALL
 Java_pt_b13h_derelictgles2_DerelictJNI_drawFrame(JNIEnv *env, jclass clazz) {
     if (enable3DRendering) {
-        startFrameGL(0, 0, 640, 480);
+        startFrame(0, 0, 640, 480);
 
         isRunning = isRunning && menuTick(10);
 
-        endFrameGL();
+        endFrame();
         flipRenderer();
     }
 }
