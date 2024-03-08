be5b2e8ab82322345b94a78b915ad7ac4a43dbb3
Daniel Monteiro <themontyhacker@gmail.com>
2024-03-08 08:13:42 +0000

    [GL] fix mesh rendering rotation
    Oddily, rotating to the negative value to revert the previous rotation
    would cause all sorts of issues. Rotating to 360 - a worked fine.

--- gl_frontend/GL1Common/Renderer_TesselationGL1.c
+++ gl_frontend/GL1Common/Renderer_TesselationGL1.c
@@ -573,7 +573,7 @@ void drawMesh(const struct Mesh *mesh, const struct Vec3 center, enum EDirection
     z = -fixToFloat(center.mZ + zCameraOffset);
 
     glTranslatef(x, y, z);
-
+    glRotatef(((rotation * 90)), 0.0f, 1.0f, 0.0f);
 #ifndef NDS
     glAlphaFunc(GL_GREATER, 0.5f);
 #endif
@@ -613,6 +613,6 @@ void drawMesh(const struct Mesh *mesh, const struct Vec3 center, enum EDirection
             vertexData += 9;
         }
     }
-
+    glRotatef(360-((rotation * 90)), 0.0f, 1.0f, 0.0f);
     glTranslatef(-x, -y, -z);
 }
