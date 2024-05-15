8673f98fa9c273d604a2728c6a2813e92f1459ee
Daniel Monteiro <monteiroquiet@gmail.com>
2024-05-15 18:21:49 +0100

    refactor: move the Lerp code into FixP.c

--- ee_frontend/base3d/src/es/LoadBitmap.c
+++ ee_frontend/base3d/src/es/LoadBitmap.c
@@ -93,17 +93,3 @@ void releaseBitmap(struct Bitmap *ptr) {
     disposeMem(ptr->data);
     disposeMem(ptr);
 }
-
-FixP_t lerpFix(const FixP_t v0, const FixP_t v1, const FixP_t dt, const FixP_t total) {
-    FixP_t delta = (v1 - v0);
-    FixP_t progress = Div(dt, total);
-    FixP_t reach = Mul(delta, progress);
-
-    return (v0 + reach);
-}
-
-int lerpInt(const int v0, const int v1, const long t, const long total) {
-    return fixToInt(lerpFix(intToFix(v0), intToFix(v1), intToFix(t),
-                            intToFix(total)));
-}
-
