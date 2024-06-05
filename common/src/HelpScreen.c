d500006152519a41b7f8408c3270f648ab552b27
Daniel Monteiro <themontyhacker@gmail.com>
2024-06-05 18:38:17 +0100

    feat: add basic background colour for screens that support it
    Those that don't will keep their natual background colour.
    Also notice that I've removed the forced line breaks on the strings.

--- common/src/HelpScreen.c
+++ common/src/HelpScreen.c
@@ -11,6 +11,14 @@
 #include "UI.h"
 #include "Engine.h"
 
+#ifndef MONOCHROME_VECTORS
+#include "FixP.h"
+#include "Vec.h"
+#include "Mesh.h"
+#include "CActor.h"
+#include "Renderer.h"
+#endif
+
 extern const char *mainText;
 extern int8_t cursorPosition;
 
@@ -22,12 +30,16 @@ const enum EGameMenuState HelpScreen_nextStateNavigation[1] = {
 
 void HelpScreen_initStateCallback(enum EGameMenuState tag) {
     (void)tag;
-    mainText = "You must gain access to the reactor, place the EMP\ndevice and, from a safe\ndistance, activate it.";
+    mainText = "You must gain access to the reactor, place the EMP device and, from a safe distance, activate it.";
 }
 
 void HelpScreen_repaintCallback(void) {
     if (firstFrameOnCurrentState) {
         clearScreen();
+
+#ifndef MONOCHROME_VECTORS
+        fillRect(0, 0, XRES_FRAMEBUFFER, YRES_FRAMEBUFFER, getPaletteEntry(0xFF00FF00), 0);
+#endif
 
         if (mainText != NULL) {
             drawTextWindow(1, 1, (XRES_FRAMEBUFFER / 8) - 2, 1 + 6, "Help", mainText);
