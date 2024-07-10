969d23c1c1186ddd1f298dd7a1fcd8671cbcc897
Daniel Monteiro <monteiroquiet@gmail.com>
2024-07-10 20:46:16 +0100

    [EE] Bump Android API level, but reduce minSDK

--- ee_frontend/NDK-version/app/build.gradle.kts
+++ ee_frontend/NDK-version/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "pt.b13h.derelictgles2"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "pt.b13h.derelictgles2"
-        minSdk = 22
-        targetSdk = 34
+        minSdk = 21
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
